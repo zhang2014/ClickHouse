de1d00baac360cef9c95b03e8ff8aa61755aa3d2
zhang2014 <coswde@gmail.com>
2018-08-06 15:23:13 +0800

    Add type column for system settings

--- dbms/src/Storages/System/StorageSystemSettings.cpp
+++ dbms/src/Storages/System/StorageSystemSettings.cpp
@@ -11,6 +11,7 @@ NamesAndTypesList StorageSystemSettings::getNamesAndTypes()
 {
     return {
         {"name", std::make_shared<DataTypeString>()},
+        {"type", std::make_shared<DataTypeString>()},
         {"value", std::make_shared<DataTypeString>()},
         {"changed", std::make_shared<DataTypeUInt8>()},
         {"description", std::make_shared<DataTypeString>()},
@@ -23,9 +24,10 @@ void StorageSystemSettings::fillData(MutableColumns & res_columns, const Context
 
 #define ADD_SETTING(TYPE, NAME, DEFAULT, DESCRIPTION)      \
     res_columns[0]->insert(String(#NAME));                 \
-    res_columns[1]->insert(settings.NAME.toString());      \
-    res_columns[2]->insert(UInt64(settings.NAME.changed)); \
-    res_columns[3]->insert(String(DESCRIPTION));
+    res_columns[1]->insert(String(#TYPE));                 \
+    res_columns[2]->insert(settings.NAME.toString());      \
+    res_columns[3]->insert(UInt64(settings.NAME.changed)); \
+    res_columns[4]->insert(String(DESCRIPTION));
     APPLY_FOR_SETTINGS(ADD_SETTING)
 #undef ADD_SETTING
 }
