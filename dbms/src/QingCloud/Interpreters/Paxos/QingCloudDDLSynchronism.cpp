ee7c66ffd53b76c78a713484132019df9cbca49e
zhang2014 <coswde@gmail.com>
2018-11-24 22:14:22 +0800

    tmp

--- dbms/src/QingCloud/Interpreters/Paxos/QingCloudDDLSynchronism.cpp
+++ dbms/src/QingCloud/Interpreters/Paxos/QingCloudDDLSynchronism.cpp
@@ -108,32 +108,4 @@ Block QingCloudDDLSynchronism::acceptedProposal(const String &from, const UInt64
 
 QingCloudDDLSynchronism::~QingCloudDDLSynchronism() = default;
 
-QingCloudDDLSynchronism::DDLEntity::DDLEntity(const String & data_path_)
-        :dir(data_path_), data_path(data_path_ + "paxos.info")
-{
-    if (Poco::File(data_path).exists())
-    {
-        ReadBufferFromFile buffer(data_path);
-        readBinary(applied_paxos_id, buffer);
-        readBinary(applied_entity_id, buffer);
-        readBinary(accepted_paxos_id, buffer);
-        readBinary(accepted_entity_id, buffer);
-        readStringBinary(accepted_entity_value, buffer);
-    }
-
-    store();
-}
-
-void QingCloudDDLSynchronism::DDLEntity::store()
-{
-    Poco::File(dir).createDirectories();
-    WriteBufferFromFile buffer(data_path);
-    writeBinary(applied_paxos_id, buffer);
-    writeBinary(applied_entity_id, buffer);
-    writeBinary(accepted_paxos_id, buffer);
-    writeBinary(accepted_entity_id, buffer);
-    writeStringBinary(accepted_entity_value, buffer);
-    buffer.sync();
-}
-
 }
