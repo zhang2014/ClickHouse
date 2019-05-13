dd312fcca42449a229730b5fde45b0fb8377bb18
zhang2014 <coswde@gmail.com>
2019-05-13 12:37:11 +0800

    fix rename data race

--- dbms/tests/queries/0_stateless/00944_rename_data_race.sh
+++ dbms/tests/queries/0_stateless/00944_rename_data_race.sh
@@ -5,17 +5,18 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 set -e
 
-$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS alter_table"
-$CLICKHOUSE_CLIENT -q "CREATE TABLE alter_table (a UInt8, b Int16, c Float32, d String, e Array(UInt8), f Nullable(UUID), g Tuple(UInt8, UInt16)) ENGINE = MergeTree ORDER BY a"
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS rename_table"
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS already_rename_table"
+$CLICKHOUSE_CLIENT -q "CREATE TABLE rename_table (a UInt8, b Int16, c Float32, d String, e Array(UInt8), f Nullable(UUID), g Tuple(UInt8, UInt16)) ENGINE = MergeTree ORDER BY a"
 
 function thread1()
 {
-    while true; do $CLICKHOUSE_CLIENT --query "RENAME TABLE alter_table TO rename_alter_table"; done
+    while true; do $CLICKHOUSE_CLIENT --query "RENAME TABLE rename_table TO already_rename_table"; done
 }
 
 function thread2()
 {
-    while true; do $CLICKHOUSE_CLIENT -n --query "RENAME TABLE rename_alter_table TO alter_table"; done
+    while true; do $CLICKHOUSE_CLIENT -n --query "RENAME TABLE already_rename_table TO rename_table"; done
 }
 
 # https://stackoverflow.com/questions/9954794/execute-a-shell-function-with-timeout
@@ -41,4 +42,5 @@ timeout 15 bash -c thread2 2> /dev/null &
 
 wait
 
-$CLICKHOUSE_CLIENT -q "DROP TABLE alter_table"
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS rename_table"
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS already_rename_table"
