d86f1a318fc3dc2d52406ba634d38f901144ee4a
zhang2014 <coswde@gmail.com>
2020-05-12 17:38:00 +0800

    ISSUES-4006 fix create query convert

--- src/Databases/DatabaseFactory.cpp
+++ src/Databases/DatabaseFactory.cpp
@@ -65,6 +65,7 @@ static inline ValueType safeGetLiteralValue(const ASTPtr &ast, const String &eng
     return ast->as<ASTLiteral>()->value.safeGet<ValueType>();
 }
 
+#if USE_MYSQL
 static inline bool materializeMySQLDatabase(const ASTSetQuery * settings)
 {
     if (!settings || settings->changes.empty())
@@ -82,6 +83,8 @@ static inline bool materializeMySQLDatabase(const ASTSetQuery * settings)
     return false;
 }
 
+#endif
+
 DatabasePtr DatabaseFactory::getImpl(
     const String & database_name, const String & metadata_path, const ASTStorage * engine_define, Context & context)
 {
@@ -91,7 +94,7 @@ DatabasePtr DatabaseFactory::getImpl(
         throw Exception("Database engine " + engine_name + " cannot have arguments", ErrorCodes::BAD_ARGUMENTS);
 
     if (engine_define->engine->parameters || engine_define->partition_by || engine_define->primary_key || engine_define->order_by ||
-        engine_define->sample_by || engine_define->settings)
+        engine_define->sample_by || (engine_name != "MySQL" && engine_define->settings))
         throw Exception("Database engine " + engine_name + " cannot have parameters, primary_key, order_by, sample_by, settings",
                         ErrorCodes::UNKNOWN_ELEMENT_IN_AST);
 
